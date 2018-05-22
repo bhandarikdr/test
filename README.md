752580862efc00d821847f259d264e9c1dc25e69
Kedar <kdr_bhandari@yahoo.com>
2018-05-22 16:27:49 +0545

    some changes in exec

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 test
 ====
 haha sahi ho.
-
 > Ok. Let's do some stuffs here. 
 great. job.
+>sahi ho //this line is added.
+
--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(void){
-printf("Hello world!\n");
+printf("Hello world!\n");//this line prints "Hellow World!"
 return 0;
 }
 //this file is updated

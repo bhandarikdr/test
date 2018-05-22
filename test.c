c6cd143f2d446967718ea9dd75be166de167cdff
Kedar <kdr_bhandari@yahoo.com>
2018-05-22 16:37:51 +0545

    add scanf function

--- test.c
+++ test.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 
 int main(void){
+int points;
 printf("Hello world!\n");//this line prints "Hellow World!"
+printf("Please giveme a point: ");
+scanf("%d",&points);
+printf("You gave me %d points. Thanks.\n", points);//displays the points.
 return 0;
 }
 //this file is updated
